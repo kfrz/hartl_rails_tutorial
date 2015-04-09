76d04dfb46e973a51d9f939445be63931420826b
Keifer Furzland <furzland@gmail.com>
2015-04-08 20:19:43 -0500

    finished admin/CRUD features

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
     format: { with: VALID_EMAIL_REGEX },
     uniqueness: { case_sensitive: false }
   has_secure_password
-  validates :password, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }, allow_blank: true
 
   # Returns the hashed digest of given string
   def self.digest(string)
