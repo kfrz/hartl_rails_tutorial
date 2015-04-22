17b159bde9bc5aa80bacf0fb421875a60638a386
Keifer Furzland <furzland@gmail.com>
2015-04-21 23:29:02 -0500

    implemented user following

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-
+# users
 User.create!(name:  "Example User",
              email: "example@example.com",
              password: "foobar",
@@ -13,7 +7,6 @@ User.create!(name:  "Example User",
              activated: true,
              activated_at: Time.zone.now)
              
-
 99.times do |n|
   name = Faker::Name.name
   email = "example-#{n+1}@example.com"
@@ -26,10 +19,18 @@ User.create!(name:  "Example User",
                activated_at: Time.zone.now)
 end              
 
+# microposts
 users = User.order(:created_at).take(6)
 50.times do
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
-
 end
+
+# following relationships
+users = User.all
+user = users.first
+following = users[2..40]
+followers = users[3..30]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
     
